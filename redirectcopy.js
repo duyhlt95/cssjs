1b9449c5227e612d635d61223fd52b464e2b52d8
duyhlt95 <101549573+duyhlt95@users.noreply.github.com>
2022-03-14 16:31:30 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://camerakienphong.blogspot.com/'>https://camerakienphong.blogspot.com/</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://congtycamera.com/'>https://congtycamera.com/</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
